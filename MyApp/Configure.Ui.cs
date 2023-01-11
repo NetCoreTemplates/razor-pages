66c93dec4caeadc2daf1c8963e9ca3f41786880f
Demis Bellot <demis.bellot@gmail.com>
2023-01-11 09:27:03 +0800

    Revert to .NET 6.0 and add runtime razor compilation

--- MyApp/Configure.Ui.cs
+++ MyApp/Configure.Ui.cs
@@ -1,6 +1,6 @@
-using ServiceStack;
-using ServiceStack.Mvc;
 using System.Net;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyApp.ServiceModel.Types;
 
 [assembly: HostingStartup(typeof(MyApp.ConfigureUi))]
 
@@ -10,17 +10,41 @@ public class ConfigureUi : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
         .ConfigureAppHost(appHost => {
-            appHost.CustomErrorHttpHandlers[HttpStatusCode.NotFound] = new RazorHandler("/notfound");
+            appHost.CustomErrorHttpHandlers[HttpStatusCode.NotFound] = new RazorHandler("/NotFound");
             appHost.CustomErrorHttpHandlers[HttpStatusCode.Forbidden] = new RazorHandler("/Forbidden");
             
+            // Populates @Html.Navbar() menu
             View.NavItems.AddRange(new List<NavItem> {
                 new() { Href = "/",         Label = "Home",    Exact = true },
                 new() { Href = "/Privacy",  Label = "Privacy" },
                 new() { Href = "/TodoMvc",  Label = "Todo Mvc" },
-                new() { Href = "/SignIn",   Label = "Sign In", Hide = "auth" },
-                new() { Href = "/Profile",  Label = "Profile", Show = "auth" },
+                new() { Href = "/SignIn",   Label = "Sign In",  Hide = "auth" },
+                new() { Href = "/Profile",  Label = "Profile",  Show = "auth" },
                 new() { Href = "/Contacts", Label = "Contacts", Show = "auth" },
-                new() { Href = "/Admin",    Label = "Admin",   Show = "role:Admin" },
+                new() { Href = "/Admin",    Label = "Admin",    Show = "role:Admin" },
             });
         });
+}
+
+public class AppData
+{
+    public readonly Dictionary<string, string> Colors = new() {
+        {"#ffa4a2", "Red"},
+        {"#b2fab4", "Green"},
+        {"#9be7ff", "Blue"}
+    };
+    public readonly List<string> FilmGenres = EnumUtils.GetValues<FilmGenres>().Map(x => x.ToDescription());
+
+    public readonly List<KeyValuePair<string, string>> Titles = EnumUtils.GetValues<Title>()
+        .Where(x => x != Title.Unspecified)
+        .ToKeyValuePairs();
+}
+
+public static class AppDataExtensions
+{
+    internal static readonly AppData Instance = new();
+
+    public static Dictionary<string, string> ContactColors(this IHtmlHelper html) => Instance.Colors;
+    public static List<KeyValuePair<string, string>> ContactTitles(this IHtmlHelper html) => Instance.Titles;
+    public static List<string> ContactGenres(this IHtmlHelper html) => Instance.FilmGenres;
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,12 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+var mvcBuilder = builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    mvcBuilder.AddRazorRuntimeCompilation();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
